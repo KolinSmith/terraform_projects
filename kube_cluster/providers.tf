2d6556933a3ed6fd300110a446064942284e823c
Kolin Smith <kolinksmith@gmail.com>
2024-12-23 11:41:07 -0600

    adding in a providers file

---
+++ kube_cluster/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.14"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = "https://discovery.internal.homelab.gg:8006/api2/json"
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure     = true
+}
